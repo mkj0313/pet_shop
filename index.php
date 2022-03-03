501290c94f84850d3a86710b093c31bc382c8626
mkj0313 <mk3131985@yahoo.co.jp>
2022-03-03 23:55:01 +0900

    ペットショップ2課題完了

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
+$keyword = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $keyword = $_POST['keyword'];
+}
+
 // 関数ファイルを読み込む
 require_once __DIR__ . '/functions.php';
 
@@ -10,10 +16,18 @@ $dbh = connect_db();
 $sql = <<<EOM
 SELECT * FROM
     animals
+WHERE description LIKE :description
 EOM;
 
 // プリペアドステートメントの準備
 $stmt = $dbh->prepare($sql);
+
+// バインドするパラメータの準備(更新情報の準備)
+$description = '%' . $keyword . '%';
+
+// パラメータのバインド
+$stmt->bindParam(':description', $description, PDO::PARAM_STR);
+
 // プリペアドステートメントの実行
 $stmt->execute();
 
@@ -33,16 +47,23 @@ $animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 <body>
     <h2>本日のご紹介ペット！</h2>
-    <ul>
-        <?php foreach ($animals as $animal) : ?>
-            <p><?= h($animal['type']) . 'の' . h($animal['classification']) . 'ちゃん' ?><br>
-                <?= h($animal['description'])  ?><br>
-                <?= h($animal['birthday']) . ' 生まれ'  ?><br>
-                <?= '出生地 ' . h($animal['birthplace'])  ?><br>
-            </p>
-            <hr>
-        <?php endforeach; ?>
-    </ul>
+    <form action="" method="post">
+        <div>
+            <label for="keyword">キーワード</label>
+            <input type="text" id="keyword" name="keyword">
+            <input type="submit" value="検索">
+        </div>
+    </form>
+    <!-- <ul> -->
+    <?php foreach ($animals as $animal) : ?>
+        <p><?= h($animal['type']) . 'の' . h($animal['classification']) . 'ちゃん' ?><br>
+            <?= h($animal['description'])  ?><br>
+            <?= h($animal['birthday']) . ' 生まれ'  ?><br>
+            <?= '出生地 ' . h($animal['birthplace'])  ?><br>
+        </p>
+        <hr>
+    <?php endforeach; ?>
+    <!-- </ul> -->
 </body>
 
 </html>
